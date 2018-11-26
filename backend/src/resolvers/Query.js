99676f78218211f1a460f33be8c50640e2847d0f
Christopher Bradley <ctb2oo5@gmail.com>
2018-11-25 20:38:36 -0500

    Add user orders

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -24,6 +24,39 @@ const Query = {
 
         return ctx.db.query.users({}, info)
     },
+    order: async (_, args, ctx, info) => {
+        if (!ctx.request.userId) {
+            throw new Error('You must be logged in!')
+        }
+        const order = await ctx.db.query.order(
+            {
+                where: { id: args.id },
+            },
+            info
+        )
+        const ownsOrder = order.user.id === ctx.request.userId
+        const hasPermissionToSeeOrder = ctx.request.user.permissions.includes(
+            'ADMIN'
+        )
+        if (!ownsOrder || !hasPermissionToSeeOrder) {
+            throw new Error("You don't have permission to see this order!")
+        }
+        return order
+    },
+    orders: async (_, args, ctx, info) => {
+        const { userId } = ctx.request
+        if (!userId) {
+            throw new Error('You must be logged in!')
+        }
+        return ctx.db.query.orders(
+            {
+                where: {
+                    user: { id: userId },
+                },
+            },
+            info
+        )
+    },
 }
 
 module.exports = Query
