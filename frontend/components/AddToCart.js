147bec2e652e6d3ba15ed1aea8df9634e50f3b41
Christopher Bradley <ctb2oo5@gmail.com>
2018-12-08 16:50:26 -0500

    finished testing

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -3,7 +3,7 @@ import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
 import { CURRENT_USER_QUERY } from './User'
 
-const ADD_TO_CART = gql`
+const ADD_TO_CART_MUTATION = gql`
     mutation addToCart($id: ID!) {
         addToCart(id: $id) {
             id
@@ -17,7 +17,7 @@ class AddToCart extends React.Component {
         const { id } = this.props
         return (
             <Mutation
-                mutation={ADD_TO_CART}
+                mutation={ADD_TO_CART_MUTATION}
                 variables={{ id }}
                 refetchQueries={[{ query: CURRENT_USER_QUERY }]}
             >
@@ -32,3 +32,4 @@ class AddToCart extends React.Component {
 }
 
 export default AddToCart
+export { ADD_TO_CART_MUTATION }
