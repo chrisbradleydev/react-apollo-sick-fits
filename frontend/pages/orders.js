d06726c13e675044531b8157b89f67f520c00030
Christopher Bradley <ctb2oo5@gmail.com>
2018-12-08 14:07:23 -0500

    update PleaseSignin component

--- frontend/pages/orders.js
+++ frontend/pages/orders.js
@@ -1,5 +1,5 @@
-import PleaseSignIn from '../components/PleaseSignIn'
 import OrderList from '../components/OrderList'
+import PleaseSignIn from '../components/PleaseSignin'
 
 const OrderPage = props => (
     <div>
